eeaede62b48698354ce5838754e64f0118568758
Manish <manishkasera456@gmail.cm>
2021-10-28 20:01:03 +0530

    remove recursion file

--- src/com/mani/Recursion/string/Assci.java
+++
@@ -1,8 +0,0 @@
-package com.mani.Recursion.string;
-
-public class Assci {
-    public static void main(String[] args) {
-        char ch ='a';
-        System.out.println(ch + 0) ;
-    }
-}
--- src/com/mani/Revision/Second.java
+++
@@ -1,8 +0,0 @@
-package com.mani.Revision;
-
-public class Second {
-    public static void main(String[] args) {
-        Human vjya = new Human();
-        System.out.println(vjya.hand);
-    }
-}
